d8bd4a7111bf2140119bbbd8e3826df53526ec7c
Yash Ladia <ladiayash@gmail.com>
2023-05-21 09:44:48 +0530

    Created repository with dependency and main application class

---
+++ src/main/java/com/tigerhall/TigerSightingApplication.java
@@ -0,0 +1,11 @@
+package com.tigerhall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerSightingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TigerSightingApplication.class, args);
+    }
+}
